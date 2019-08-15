03616512dd32ef5877bc3af790dcf8e53113f13c
Rob Nelson <robert.nelson@duke.edu>
2019-08-15 14:37:06 -0400

    flesh out some details of the person profile page

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,5 +1,5 @@
-//require("expose-loader?$!expose-loader?jQuery!jquery");
-//require("bootstrap/dist/js/bootstrap.bundle.js");
+require("expose-loader?$!expose-loader?jQuery!jquery");
+require("bootstrap/dist/js/bootstrap.bundle.js");
 //require("bootstrap-sass/assets/javascripts/bootstrap.js");
 //import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter'
 //import '@webcomponents/webcomponentsjs/webcomponents-loader.js'
