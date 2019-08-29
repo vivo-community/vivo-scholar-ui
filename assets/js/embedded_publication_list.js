81a72ade5e7e0e51b993c58c03a02ba8fb8c2974
Jim Wood <jim.wood@duke.edu>
2019-08-29 17:06:14 -0400

    Add embedded pub list demo
    Make web component that will retrieve and display a publication list
    using display components defined in the app.
    
    Add static web page to demonstrate usage.

---
+++ assets/js/embedded_publication_list.js
@@ -0,0 +1,54 @@
+import './web-components/publication'
+import './web-components/publication-list'
+import gql from 'graphql-tag'
+import client from './lib/apollo'
+
+const PUBLICATION_QUERY = gql`
+  query($id: String) {
+    person(id: $id) {
+      id
+      selectedPublications {
+        id
+        title
+        abstractText
+        doi
+        publicationDate
+        authors {
+          id
+          label
+        }
+      }
+    }
+  }
+`
+
+class EmbeddedPublicationList extends HTMLElement {
+
+  constructor() {
+    super();
+    const shadowRoot = this.attachShadow({mode: 'open'})
+  }
+
+  connectedCallback() {
+    const data = client.query({
+      query: PUBLICATION_QUERY,
+      variables: {
+        id: this.getAttribute("person_id")
+      }
+    }).then(({data}) =>  {
+      let publications = data.person.selectedPublications
+      let publicationElements = publications.map(p => {
+        let pub = document.createElement('vivo-publication');
+        pub.setAttribute('id',p.id);
+        pub.setAttribute('title',p.title);
+        return pub;
+      })
+      let publicationList = document.createElement('vivo-publication-list');
+      publicationElements.forEach(p => publicationList.appendChild(p));
+      this.shadowRoot.appendChild(publicationList);
+    });
+  }
+
+}
+
+customElements.define('vivo-embedded-publication-list', EmbeddedPublicationList)
