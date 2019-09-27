7c3210abc518fe5c07417b8a01c22f0000b2a230
Jim Wood <jim.wood@duke.edu>
2019-09-27 17:02:34 -0400

    Convert Publication to LitElement

--- assets/js/elements/publication-list.js
+++ assets/js/elements/publication-list.js
@@ -1,23 +1,28 @@
-class PublicationList extends HTMLElement {
-  constructor() {
-    super();
-    const shadowRoot = this.attachShadow({mode: 'open'});
-    shadowRoot.innerHTML =  `
-        <style>
-          :host {
-            display: block;
-          }
-          ::slotted(vivo-publication) {
-            margin-bottom: 1em;
-          }
-          ::slotted(vivo-publication) {
-            font-size: var(--publication-font-size, 18px);
-            border: 1px solid lightgray;
-          }
-        </style>
-        <slot />
+import { LitElement, html, css } from 'lit-element';
+
+class PublicationList extends LitElement {
+
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+      }
+      ::slotted(vivo-publication) {
+        margin-bottom: 1em;
+      }
+      ::slotted(vivo-publication) {
+        font-size: var(--publication-font-size, 18px);
+        border: 1px solid lightgray;
+      }
+    `
+  }
+
+  render() {
+    return html`
+      <slot />
     `;
   }
+
 }
 
-customElements.define('vivo-publication-list', PublicationList)
+customElements.define('vivo-publication-list', PublicationList);
