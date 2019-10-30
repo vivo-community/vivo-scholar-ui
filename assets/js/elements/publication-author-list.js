392a115448b7329aa2024aca1b01c8d5a93f4c44
Rob Nelson <robert.nelson@duke.edu>
2019-10-30 11:21:09 -0400

    #6: take out code that would toggle author list

--- assets/js/elements/publication-author-list.js
+++ assets/js/elements/publication-author-list.js
@@ -9,8 +9,7 @@ class PublicationAuthorList extends LitElement {
         reflect: true
       },
       authorCount: { type: Number },
-      truncatedAuthorCount: { type: Number },
-      authorsExpanded: { type: Boolean }
+      truncatedAuthorCount: { type: Number }
     };
   }
 
@@ -20,7 +19,6 @@ class PublicationAuthorList extends LitElement {
     this.authorCount = 0;
     this.truncatedAuthorCount = 0;
     this.slotChanged = this.slotChanged.bind(this);
-    this.authorsExpanded = false;
   }
 
   firstUpdated() {
@@ -50,40 +48,7 @@ class PublicationAuthorList extends LitElement {
     `;
   }
 
-  authorsToggle(e) {
-    if (this.authorsExpanded) {
-      console.log("should hide author list");
-    } else {
-      console.log("should show author list");
-    }
-    this.authorsExpanded = !this.authorsExpanded;
-  }
-
   render() {
-    // 1. if truncatedAuthorCount > 0 
-    //    a. if authorsExpanded -> show whole list -> @click to collapse
-    //    b. else -> show truncated list -> @click to expand
-    // 2. else -> show list (no @click)
-    //
-    // not sure how to get 'original' content?
-    // let slot = this.shadowRoot.querySelector("slot");
-    // let postContent = () => {
-    //  if(truncatedAuthorCount > 0) {
-    //     if (this.authorsExpanded) {
-    //       return `
-    //         <span class="all-authors" @click="${this.authorToggle}"
-    //           >${slot}
-    //         >`
-    //     } else {
-    //       return `
-    //         <span class="truncated-authors" @click="${this.authorToggle}"
-    //           >+ ${this.truncatedAuthorCount} authors</span
-    //          >`
-    //     }
-    //  } else {
-    //     return null;
-    //  }
-    //}
     return html`
       <style type="text/css">
         ::slotted(vivo-publication-author:nth-child(n
@@ -98,7 +63,7 @@ class PublicationAuthorList extends LitElement {
       <slot></slot>
       ${this.truncatedAuthorCount > 0
         ? html`
-            <span class="truncated-authors" @click="${this.authorsToggle}"
+            <span class="truncated-authors"
               >+ ${this.truncatedAuthorCount} authors</span
             >
           `
