48dac6d8ce86e7bbb3b91815cdad33474ac814bc
Rob Nelson <robert.nelson@duke.edu>
2019-10-31 10:48:05 -0400

    #6: add non-working code to try and truncate publication abstract

--- assets/js/elements/publication-abstract.js
+++ assets/js/elements/publication-abstract.js
@@ -3,8 +3,40 @@ import { LitElement, html, css } from "lit-element";
 class PublicationAbstract extends LitElement {
   static get properties() {
     return {
-      maxLength: { attribute: "max-length", type: Number }
+      maxLength: { attribute: "max-length", type: Number },
+      fullText: { type: String },
+      truncatedText: { type: String }
     };
+  }
+
+  constructor() {
+    super();
+    this.fullText = "";
+    this.truncatedText = "this should be the truncated text";
+    this.maxLength = 5; // low just to make it obvious
+    this.slotChanged = this.slotChanged.bind(this);
+    // doesn't seem to be called with window resize
+    this.onResize = this.onResize.bind(this);
+  }
+
+  firstUpdated(props) {
+    var shadow = this.shadowRoot
+    const slot = shadow.getElementById("pub-abstract");
+    if (slot.assignedNodes().length > 0) {
+        console.log(`text=${slot.assignedNodes()[0].textContent}`);
+        // NOTE: this assumes it is ALL text (e.g. no elements)
+        // abstract can have html in them though ...
+        this.fullText = slot.assignedNodes()[0].textContent;
+    }
+  }
+
+  onResize(e) {
+    console.log("publication-abstract resized");
+  }
+
+  slotChanged(e) {
+    console.log("publication-abstract slot changed");
+    //console.log(`slotChanged:innerHTML=${e.target.innerHTML[0]}`);
   }
 
   static get styles() {
@@ -16,8 +48,19 @@ class PublicationAbstract extends LitElement {
   }
 
   render() {
+    console.log(`truncate?=${this.fullText.length > this.maxLength}`);
+    console.log(`text-length=${this.fullText.length}`)
+    console.log(`max-length=${this.maxLength}`);
     return html`
-      <slot></slot>
+      ${this.fullText.length > this.maxLength
+      ? html`
+        <slot id="pub-abstract">${this.truncatedText} <span>... (More)</span></slot>
+        ` 
+        : 
+        html`
+        <slot id="pub-abstract">${this.fullText}</slot>
+        `
+      }
     `;
   }
 }
@@ -37,6 +80,18 @@ TODO:
 e.g.
 html`<span>...</span><a @click="${this.clickHandler}">(More)</a>`
 
+    ${this.truncatedPubCount > 0
+        ? html`
+          <div class="pub-summary">
+          <span class="pubs-shown-message"
+            >Showing ${this.displayedPubCount} featured publications
+          </span>
+          <span class="show-all-pubs" @click="${this.showAllPubs}"
+            >Show all ${this.truncatedPubCount} publications ></span
+          >
+          </div>
+        `
+        : null}
 
 
 */
