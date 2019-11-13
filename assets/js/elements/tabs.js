eddf7ad6f41746049e8cd0b8dfb14ab4a8eca6c6
Rob Nelson <robert.nelson@duke.edu>
2019-11-13 15:57:47 -0500

    #21: improve accordian look for tabs

--- assets/js/elements/tabs.js
+++ assets/js/elements/tabs.js
@@ -2,74 +2,74 @@ import { LitElement, html, css } from 'lit-element';
 
 class Tabs extends LitElement {
 
-    static get properties() {
-        return {
-            vivoTabStyle: { attribute: 'vivo-tab-style', type: String, reflect: true }
-        }
+  static get properties() {
+    return {
+      vivoTabStyle: { attribute: 'vivo-tab-style', type: String, reflect: true }
     }
+  }
 
-    constructor() {
-        super();
-        this.tabs = [];
-        this.panels = [];
-        this._onSlotChange = this._onSlotChange.bind(this);
-        this.vivoTabStyle = "primary";
+  constructor() {
+    super();
+    this.tabs = [];
+    this.panels = [];
+    this._onSlotChange = this._onSlotChange.bind(this);
+    this.vivoTabStyle = "primary";
+  }
+
+  firstUpdated() {
+    this._slot = this.shadowRoot.querySelector("slot");
+    this._slot.addEventListener('slotchange', this._onSlotChange);
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this._slot.removeEventListener('slotchange', this._onSlotChange);
+  }
+
+  _onSlotChange() {
+    this._linkPanels();
+  }
+
+  _linkPanels() {
+    this.tabs = Array.from(this.querySelectorAll('vivo-tab'));
+    this.panels = Array.from(this.querySelectorAll('vivo-tab-panel'));
+    this.tabs.forEach(tab => {
+      const panel = this.panels[this.tabs.indexOf(tab)];
+      if (panel) {
+        tab.setAttribute('aria-controls', panel.id);
+        tab.vivoTabStyle = this.vivoTabStyle;
+        panel.setAttribute('aria-labelledby', tab.id);
+        panel.vivoTabStyle = this.vivoTabStyle;
+      }
+    });
+  }
+
+  selectTab(e) {
+    let tab = e.target;
+    while (tab) {
+      if (tab.matches('vivo-tab')) {
+        break;
+      } else if (tab.matches('body')) {
+        tab = null;
+        break;
+      } else {
+        tab = tab.parentElement;
+      }
     }
-
-    firstUpdated() {
-        this._slot = this.shadowRoot.querySelector("slot");
-        this._slot.addEventListener('slotchange',this._onSlotChange);
+    if (tab) {
+      let tabs = this.querySelectorAll('vivo-tab');
+      tabs.forEach((t) => t.removeAttribute('selected'));
+      tab.setAttribute('selected', 'selected');
+      let index = Array.from(tabs).indexOf(tab);
+      let panels = this.querySelectorAll('vivo-tab-panel');
+      panels.forEach((t) => t.removeAttribute('selected'));
+      panels[index].setAttribute('selected', 'selected');
     }
+  }
 
-    disconnectedCallback() {
-        super.disconnectedCallback();
-        this._slot.removeEventListener('slotchange',this._onSlotChange);
-    }
-
-    _onSlotChange() {
-        this._linkPanels();
-    }
-
-    _linkPanels() {
-        this.tabs = Array.from(this.querySelectorAll('vivo-tab'));
-        this.panels = Array.from(this.querySelectorAll('vivo-tab-panel'));
-        this.tabs.forEach(tab => {
-            const panel = this.panels[this.tabs.indexOf(tab)];
-            if (panel) {
-                tab.setAttribute('aria-controls', panel.id);
-                tab.vivoTabStyle = this.vivoTabStyle;
-                panel.setAttribute('aria-labelledby', tab.id);
-                panel.vivoTabStyle = this.vivoTabStyle;
-            }
-        });
-    }
-
-    selectTab(e) {
-        let tab = e.target;
-        while (tab) {
-            if (tab.matches('vivo-tab')) {
-                break;
-            } else if (tab.matches('body')) {
-                tab = null;
-                break;
-            } else {
-                tab = tab.parentElement;
-            }
-        }
-        if (tab) {
-            let tabs = this.querySelectorAll('vivo-tab');
-            tabs.forEach((t) => t.removeAttribute('selected'));
-            tab.setAttribute('selected', 'selected');
-            let index = Array.from(tabs).indexOf(tab);
-            let panels = this.querySelectorAll('vivo-tab-panel');
-            panels.forEach((t) => t.removeAttribute('selected'));
-            panels[index].setAttribute('selected', 'selected');
-        }
-    }
-
-    static get styles() {
-        return css`
-    :host {
+  static get styles() {
+    return css`
+      :host {
         display: flex;
         flex-wrap: wrap;
       }  
@@ -125,24 +125,30 @@ class Tabs extends LitElement {
           width: 100%;
           margin-right: 0;
           margin-top: 0.2rem;
+          background-color: var(--highlightBackgroundColor);
+        }
+        :host([vivo-tab-style="secondary"]) ::slotted(vivo-tab) {
+          color: var(--textColor);
+          border-bottom: 1px solid var(--textColor);
         }
         ::slotted(vivo-tab-panel), ::slotted(vivo-tab) {
             order: initial;
-          }
-          ::slotted(vivo-tab-panel) {
-            width: 100%;
-            margin-right: 0;
-            margin-top: 0.2rem;
-          }
+        }
+        ::slotted(vivo-tab-panel) {
+          width: 100%;
+          margin-right: 0;
+          margin-top: 0.2rem;
+          border-top: 0px;
+        }
       }     
     `
-    }
+  }
 
-    render() {
-        return html`
+  render() {
+    return html`
         <slot @click="${this.selectTab}"/>
     `
-    }
+  }
 }
 
 customElements.define('vivo-tabs', Tabs);
