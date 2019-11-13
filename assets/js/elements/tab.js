86467d757a7d48a02f3315ebd92eb370d628be31
Rob Nelson <robert.nelson@duke.edu>
2019-11-13 15:49:03 -0500

    #21: rework tab control to work with insert order

--- assets/js/elements/tab.js
+++ assets/js/elements/tab.js
@@ -1,59 +1,7 @@
 import { LitElement, html, css } from 'lit-element';
 
 class Tab extends LitElement {
-
-  static get properties() {
-    return {
-      vivoTabStyle: { attribute: 'vivo-tab-style', type: String, reflect: true }
-    }
-  }
-
-  static get styles() {
-    return css`
-      :host {
-        display: inline-block;
-        box-sizing: border-box;
-        cursor: pointer;
-        margin: 0;
-      }
-      :host([vivo-tab-style="primary"]) {
-        padding: 1em;
-      }
-      :host([vivo-tab-style="secondary"]) {
-        padding: .5em;
-      }
-      :host([selected][vivo-tab-style="primary"]) {
-        background-color: var(--highlightBackgroundColor);
-      }
-      :host([vivo-tab-style="primary"]:hover) {
-        background-color: var(--highlightBackgroundColor);
-      }
-      :host([vivo-tab-style="secondary"]) {
-        background-color: transparent;
-        color: var(--darkNeutralColor);
-      }
-      :host([selected][vivo-tab-style="secondary"]) {
-        color: var(--textColor);
-        border-bottom: 1px solid var(--textColor);
-      }
-      :host([vivo-tab-style="secondary"]:hover) {
-        color: var(--textColor);
-        border-bottom: 1px solid var(--textColor);
-      }
-
-      @media screen and (max-width: 800px) {
-        :host {
-          display:flex;
-          flex-direction: column;
-          
-        }
-        :host(vivo-tab-panel:first-child) {
-          order: 2;
-        }
-      }
-    `
-  }
-
+        
   render() {
     return html`
       <slot></slot>
--- assets/js/elements/tabs.js
+++ assets/js/elements/tabs.js
@@ -2,105 +2,147 @@ import { LitElement, html, css } from 'lit-element';
 
 class Tabs extends LitElement {
 
-  static get properties() {
-    return {
-      vivoTabStyle: { attribute: 'vivo-tab-style', type: String, reflect: true }
+    static get properties() {
+        return {
+            vivoTabStyle: { attribute: 'vivo-tab-style', type: String, reflect: true }
+        }
     }
-  }
 
-  constructor() {
-    super();
-    this.tabs = [];
-    this.panels = [];
-    this._onSlotChange = this._onSlotChange.bind(this);
-    this.vivoTabStyle = "primary";
-  }
-
-  firstUpdated() {
-    this._tabSlot = this.shadowRoot.querySelector('slot[name="tabs"]');
-    this._panelSlot = this.shadowRoot.querySelector('slot[name="panels"]');
-    this._tabSlot.addEventListener('slotchange',this._onSlotChange);
-    this._panelSlot.addEventListener('slotchange',this._onSlotChange);
-  }
-
-  disconnectedCallback() {
-    super.disconnectedCallback();
-    this._tabSlot.removeEventListener('slotchange',this._onSlotChange);
-    this._panelSlot.removeEventListener('slotchange',this._onSlotChange);
-  }
-
-  _onSlotChange() {
-    this._linkPanels();
-  }
-
-  _linkPanels() {
-    this.tabs = Array.from(this.querySelectorAll('vivo-tab'));
-    this.panels = Array.from(this.querySelectorAll('vivo-tab-panel'));
-    this.tabs.forEach(tab => {
-      const panel = this.panels[this.tabs.indexOf(tab)];
-      if (panel) {
-        tab.setAttribute('aria-controls', panel.id);
-        tab.vivoTabStyle = this.vivoTabStyle;
-        panel.setAttribute('aria-labelledby', tab.id);
-        panel.vivoTabStyle = this.vivoTabStyle;
-      }
-    });
-  }
-
-  selectTab(e) {
-    let tab = e.target;
-    while (tab) {
-      if (tab.matches('vivo-tab')) {
-        break;
-      } else if (tab.matches('body')) {
-        tab = null;
-        break;
-      } else {
-        tab = tab.parentElement;
-      }
+    constructor() {
+        super();
+        this.tabs = [];
+        this.panels = [];
+        this._onSlotChange = this._onSlotChange.bind(this);
+        this.vivoTabStyle = "primary";
     }
-    if (tab) {
-      let tabs = this.querySelectorAll('vivo-tab');
-      tabs.forEach((t) => t.removeAttribute('selected'));
-      tab.setAttribute('selected','selected');
-      let index = Array.from(tabs).indexOf(tab);
-      let panels = this.querySelectorAll('vivo-tab-panel');
-      panels.forEach((t) => t.removeAttribute('selected'));
-      panels[index].setAttribute('selected','selected');
-    }
-  }
 
-  static get styles() {
-    return css`
-      :host {
+    firstUpdated() {
+        this._slot = this.shadowRoot.querySelector("slot");
+        this._slot.addEventListener('slotchange',this._onSlotChange);
+    }
+
+    disconnectedCallback() {
+        super.disconnectedCallback();
+        this._slot.removeEventListener('slotchange',this._onSlotChange);
+    }
+
+    _onSlotChange() {
+        this._linkPanels();
+    }
+
+    _linkPanels() {
+        this.tabs = Array.from(this.querySelectorAll('vivo-tab'));
+        this.panels = Array.from(this.querySelectorAll('vivo-tab-panel'));
+        this.tabs.forEach(tab => {
+            const panel = this.panels[this.tabs.indexOf(tab)];
+            if (panel) {
+                tab.setAttribute('aria-controls', panel.id);
+                tab.vivoTabStyle = this.vivoTabStyle;
+                panel.setAttribute('aria-labelledby', tab.id);
+                panel.vivoTabStyle = this.vivoTabStyle;
+            }
+        });
+    }
+
+    selectTab(e) {
+        let tab = e.target;
+        while (tab) {
+            if (tab.matches('vivo-tab')) {
+                break;
+            } else if (tab.matches('body')) {
+                tab = null;
+                break;
+            } else {
+                tab = tab.parentElement;
+            }
+        }
+        if (tab) {
+            let tabs = this.querySelectorAll('vivo-tab');
+            tabs.forEach((t) => t.removeAttribute('selected'));
+            tab.setAttribute('selected', 'selected');
+            let index = Array.from(tabs).indexOf(tab);
+            let panels = this.querySelectorAll('vivo-tab-panel');
+            panels.forEach((t) => t.removeAttribute('selected'));
+            panels[index].setAttribute('selected', 'selected');
+        }
+    }
+
+    static get styles() {
+        return css`
+    :host {
+        display: flex;
+        flex-wrap: wrap;
+      }  
+      ::slotted(vivo-tab) {
+        order: 1;
         display: block;
-        box-sizing: border-box;
+        cursor: pointer;
+        padding: 1rem 2rem;
       }
+      ::slotted(vivo-tab[selected]) {
+        background-color: var(--highlightBackgroundColor);
+      }
+      ::slotted(vivo-tab:hover) {
+        background-color: var(--highlightBackgroundColor);
+      }
+      :host([vivo-tab-style="secondary"]) ::slotted(vivo-tab) {
+        color: var(--darkNeutralColor);
+        background-color: transparent;
+      }
+      :host([vivo-tab-style="secondary"]) ::slotted(vivo-tab:hover) {
+        color: var(--textColor);
+        border-bottom: 1px solid var(--textColor);
+      }
+      :host([vivo-tab-style="secondary"]) ::slotted(vivo-tab[selected]) {
+        color: var(--textColor);
+        border-bottom: 1px solid var(--textColor);
+      }
+
       ::slotted(vivo-tab-panel) {
         display: none;
       }
       ::slotted(vivo-tab-panel[selected]) {
         display: block;
       }
-      :host([vivo-tab-style="primary"]) slot[name="tabs"] {
-        display: block;
-        margin: 0 0 .75em 0;
-        border-bottom: 10px solid var(--highlightBackgroundColor);
-      }
-      :host([vivo-tab-style="secondary"]) slot[name="tabs"] {
-        display: block;
-        margin: 0 0 .75em 0;
-        border-bottom: 1px solid var(--mediumNeutralColor);
-      }
-    `
-  }
 
-  render() {
-    return html`
-      <slot name="tabs" @click="${this.selectTab}"></slot>
-      <slot name="panels"></slot>
+      ::slotted(vivo-tab-panel) {
+        order: 99;
+        flex-grow: 1;
+        width: 100%;
+        display: none;
+        padding: 1rem;
+        background: #fafafa;
+        border-top: 10px solid var(--highlightBackgroundColor);
+      }
+      :host([vivo-tab-style="secondary"]) ::slotted(vivo-tab-panel) {
+        border-top: 1px solid var(--mediumNeutralColor);
+      }
+      @media (max-width: 45em) {
+        ::slotted(vivo-tab2) {
+          order: initial;
+        }
+        ::slotted(vivo-tab) {
+          width: 100%;
+          margin-right: 0;
+          margin-top: 0.2rem;
+        }
+        ::slotted(vivo-tab-panel), ::slotted(vivo-tab) {
+            order: initial;
+          }
+          ::slotted(vivo-tab-panel) {
+            width: 100%;
+            margin-right: 0;
+            margin-top: 0.2rem;
+          }
+      }     
     `
-  }
+    }
+
+    render() {
+        return html`
+        <slot @click="${this.selectTab}"/>
+    `
+    }
 }
 
 customElements.define('vivo-tabs', Tabs);
