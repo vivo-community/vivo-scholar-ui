7212e6fac27d0e4eca3da8360c2f52af64f531e6
Rob Nelson <robert.nelson@duke.edu>
2019-11-14 10:14:37 -0500

    #21: make responsive accordian for tabs show chevron

--- assets/js/elements/tab.js
+++ assets/js/elements/tab.js
@@ -2,8 +2,32 @@ import { LitElement, html, css } from 'lit-element';
 
 class Tab extends LitElement {
         
+
+  static get styles() {
+    return css`
+      .fas { display: none; }
+      @media (max-width: 45em) {
+        .fas {
+          display: inline-block;
+          font: normal normal normal 14px/1 FontAwesome;
+          font-size: 14px;
+          font-size: inherit;
+          text-rendering: auto
+        }
+        .fa-chevron-down::before {
+          box-sizing: border-box;
+          content: "\\f078";
+        }
+        :host([selected]) .fa-chevron-down::before {
+          content: "\\f077";
+        }
+      }
+    `
+  }
+
   render() {
     return html`
+      <i class="fas fa-chevron-down"></i>
       <slot></slot>
     `
   }
