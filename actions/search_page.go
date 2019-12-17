72f74d2edd3576c7d978aaa84437f117ec1872bb
Rob Nelson <robert.nelson@duke.edu>
2019-12-17 11:14:23 -0500

    #65: just getting rid of some commented out code

--- actions/search_page.go
+++ actions/search_page.go
@@ -1,23 +1,11 @@
 package actions
 
 import (
-	//"fmt"
-
 	"github.com/gobuffalo/buffalo"
 )
 
-// SearchPageHandler - Use naming convention to load a template
-// Template: entity_pages/{entityType}/{entityType}.html
+// SearchPageHandler - just one page?
 func SearchPageHandler(c buffalo.Context) error {
-	entityType := c.Params().Get("type")
 	viewTemplatePath := "search_pages/index.html"
-	//viewTemplatePath := fmt.Sprintf("search_pages/%s/%s.html", entityType, entityType)
-	// FIXME: send in javascript asset vue application
-	// as variable e.g. <%= javascriptTag("%type.js") %>
-	c.Set("type", entityType)
-
 	return c.Render(200, r.HTML(viewTemplatePath, "search.html"))
-
-	// NOTE: these will be javascript driven
-	//return c.Render(200, r.HTML(viewTemplatePath))
 }
