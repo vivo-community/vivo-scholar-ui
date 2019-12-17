98a584117d4b746665ec75aadd7457f471a1bd7a
Rob Nelson <robert.nelson@duke.edu>
2019-12-17 09:22:13 -0500

    #65: remove code using internal target for event bus

--- assets/js/lib/event-bus.js
+++ assets/js/lib/event-bus.js
@@ -1,20 +1,21 @@
 // https://itnext.io/handling-data-with-web-components-9e7e4a452e6e
 class EventBus {
     constructor() {
-      this._bus = document.createElement('div');
+      //this._bus = document.createElement('div');
     }
   
     register(event, callback) {
-      this._bus.addEventListener(event, callback);
+      //this._bus.addEventListener(event, callback);
       window.addEventListener(event, callback);
     } 
       
     remove(event, callback) {
-      this._bus.removeEventListener(event, callback);
+      //this._bus.removeEventListener(event, callback);
+      window.removeEventListener(event, callback);
     }  
     
     fire(evt) {
-      this._bus.dispatchEvent(evt);
+      //this._bus.dispatchEvent(evt);
       window.dispatchEvent(evt);
     }
   }
