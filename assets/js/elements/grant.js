8f427c0db515c2bc0583875e4464f5e4dc5e32bf
root <root@ab39803c0ccf>
2020-01-16 18:50:57 +0000

    added web component to style Research and Experience tab

---
+++ assets/js/elements/grant.js
@@ -0,0 +1,35 @@
+import {LitElement, html, css } from "lit-element";
+
+class Grant extends LitElement{
+
+
+  static get styles() {
+    return css `
+      :host{
+        display: block;
+        border-top: 1px solid var(--lightNeutralColor);
+        margin-bottom: 1em;
+        margin-top: 1em;
+        padding-top: 1em;
+        font-size: 18px;
+      }
+
+    ::slotted(a) {
+        color: var(--linkColor);
+        font-weight: bold;
+        text-decoration: none;
+      }
+
+    `;
+  }
+
+  render() {
+    return html `
+      <slot name="grantLabel"></slot>
+      <slot name="awardedBy"></slot>
+      <slot name="date"></slot>
+    `;
+  }
+}
+
+customElements.define("vivo-grant", Grant);
