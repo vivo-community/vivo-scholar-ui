5da62cdfee776e986d85e4c6e77ef1f1c38de3f4
Rob Nelson <robert.nelson@duke.edu>
2020-04-08 15:31:43 -0400

    #172: add different way to work through filters to remove

--- assets/js/search/facet-group.js
+++ assets/js/search/facet-group.js
@@ -60,6 +60,64 @@ class FacetGroup extends Faceter(LitElement) {
       this.waiting = true;
     }
 
+    removeNotApplicable() {
+      let grouped = _.groupBy(this.data[this.key].facets, "field");
+
+      // little wasteful
+      let allFacets = document.querySelectorAll(`vivo-search-facets, [implements="vivo-search-facets"]`);
+      let groupedFacets = _.groupBy(allFacets, "key");
+      let groupedFacets2 = _.groupBy(groupedFacets[this.key], "field");
+      //let searches = document.querySelectorAll(`[implements="vivo-search"]`);
+
+      // 2. find each vivo-facet (or implements vivo-facet) by this field
+      // 3. run it's "removeFiltersNoLongerApplicable .... method"
+      // 4. which would default to just same as below
+
+      // NOTE: this removes filters from constructed
+      // search if they are no longer in search results
+      //
+      // This would happen if another facet has been applied and
+      // narrowed the overall results
+      this.filters.map(filter => {
+        let facet = groupedFacets2[filter.field][0];
+        // first check if we even have any matches (avoid error)
+        if (grouped[filter.field]) {
+          let entries = grouped[filter.field][0].entries;
+          let content = entries.content;
+          let values = facet.getValuesFromContent(content);
+          //let values = this.getValuesFromContent(content);
+          // remove cause no longer in search results
+          if (!_.includes(values, filter.value)) {
+            // FIXME: this assumes data from facet query is same
+            // if facet.type = 'date-compare' ->
+            // else
+            // 
+            /*
+            Array(7) [ 
+                "2017-01-01T00:00:00Z", "2019-01-01T00:00:00Z", "2018-01-01T00:00:00Z", 
+                "2016-01-01T00:00:00Z", "2015-01-01T00:00:00Z", "2014-01-01T00:00:00Z", 
+                "2005-01-01T00:00:00Z" ]
+            // value [2016 TO 2021] not in array
+            removing {"field":"publicationDate","value":"[2016 TO 2021]",
+            "opKey":"BETWEEN","tag":"publicationDate"}
+            */
+            this.removeFilter(filter);
+          }
+        }
+      });
+
+    }
+
+    handleSearchResultsObtained(e) {
+      this.waiting = false;
+      const data = e.detail;
+      if (!data || !data[this.key]) {
+        return;
+      }
+      this.data = data;
+      this.removeNotApplicable();
+    }
+    /*
     handleSearchResultsObtained(e) {
       this.waiting = false;
       const data = e.detail;
@@ -87,6 +145,7 @@ class FacetGroup extends Faceter(LitElement) {
         }
       });
     }
+    */
 
     handleFacetSelected(e) {
       // ?? how to remove filters
