6862380721d478e33984918c2ab0212d2d323560
Rob Nelson <robert.nelson@duke.edu>
2020-05-14 15:17:55 -0400

    #202: possible compact mobile design

--- assets/js/search/facet-group-toggle.js
+++ assets/js/search/facet-group-toggle.js
@@ -12,6 +12,7 @@ class SearchFacetGroupToggle extends LitElement {
         super();
         this.shown = false;
         this.handleToggleFilters = this.handleToggleFilters.bind(this);
+        this.handleClearFilters = this.handleClearFilters.bind(this);
     }
 
     static get styles() {
@@ -25,6 +26,9 @@ class SearchFacetGroupToggle extends LitElement {
         a:hover {
           cursor: pointer;
         }
+
+
+
         @media screen and (max-width: 1000px) {
             :host { display: inline; }
             
@@ -35,8 +39,30 @@ class SearchFacetGroupToggle extends LitElement {
                 font-style: normal;
             }
             
+            #menu {
+                display: none;
+            }
+            #menu.open {
+                display: block;
+                color: var(--textColor);
+                background-color: var(--lightNeutralColor);  
+            }
+            #menu.open ul {
+                list-style: none;
+                padding-left: 8px;
+                padding-right: 4px;
+                margin-top: 0;
+            }
+            #menu.open ul li {
+                font-weight: bold;
+            }
+            :host ::slotted([slot="clear"]) {
+                cursor: pointer;
+                display: inline;
+            }
             :host ::slotted([slot="show"]) {
                 display: inline;
+                cursor: pointer;
             }
             :host ::slotted([slot="hide"]) {
                 display: none;
@@ -46,12 +72,35 @@ class SearchFacetGroupToggle extends LitElement {
             }
             :host([shown]) ::slotted([slot="hide"]) {
                 display: inline;
+                cursor: pointer;
             }
         }        
         `
     }
 
+    toggleOptions() {   
+        const showMenu = this.shadowRoot.querySelector("#menu");
+        if (showMenu.classList.contains('open')){
+          showMenu.classList.remove('open');
+        } else {
+          showMenu.classList.add('open');
+        }
+    }
+
+    handleClearFilters(e) {
+        this.dispatchEvent(new CustomEvent('removeFilters', {
+            detail: { clear: true }, // not sure what to put
+            bubbles: true,
+            cancelable: false,
+            composed: true
+        }));
+
+        this.toggleOptions();
+    }
+
     handleToggleFilters(e) {
+        
+        // actually show menu instead ...
         this.dispatchEvent(new CustomEvent('toggleFilters', {
             detail: { show: !this.shown }, // not sure what to put
             bubbles: true,
@@ -59,15 +108,24 @@ class SearchFacetGroupToggle extends LitElement {
             composed: true
         }));
         this.shown = !this.shown;
+
+        this.toggleOptions();
     }
 
+    //<button @click="${this.handleToggleFilters}">
     render() {
         return html`
-        <button @click="${this.handleToggleFilters}">
+        <button @click="${this.toggleOptions}">
           <i class="fas fa-filter"></i> 
-          <slot name="show"></slot>
-          <slot name="hide"></slot>
-        </button>`
+        </button>
+        <div id="menu">
+          <ul>
+            <li><slot name="show" @click="${this.handleToggleFilters}"></slot></li>
+            <li><slot name="hide" @click="${this.handleToggleFilters}"></slot></li>
+            <li><slot name="clear" @click="${this.handleClearFilters}"></slot></li>
+          </ul>
+        </div>
+        `
     }
 }
 
