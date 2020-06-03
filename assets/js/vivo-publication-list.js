8d3f4318abf48e71a0ca81847a177061dc5c8063
Rob Nelson <robert.nelson@duke.edu>
2020-06-03 11:09:00 -0400

    #248: add sort options and i18n label to embedded pub list (sort of)

--- assets/js/vivo-publication-list.js
+++ assets/js/vivo-publication-list.js
@@ -1,138 +1,13 @@
-/*
-import { LitElement, html, css } from 'lit-element';
-
-import './elements/publication';
-import './elements/publication-list';
-import './elements/publication-author-list';
-import './elements/publication-author';
-import './elements/vaadin-theme.js';
-
-import gql from 'graphql-tag'
-import ApolloClient from 'apollo-boost'
-
-let endpoint = "https://scholars-discovery-scholars.cloud.duke.edu/graphql"
-if (process.env.GRAPHQL_ENDPOINT != undefined) {
-  endpoint = `${process.env.GRAPHQL_ENDPOINT}`
-}
-
-const client = new ApolloClient({
-  uri: endpoint,
-  fetchOptions: {
-    useGETForQueries: true
-  }
-});
-
-const PUBLICATION_QUERY = gql`
-  query($id: String) {
-    person(id: $id) {
-      id
-      publications {
-        id
-        title
-        abstractText
-        doi
-        authors {
-          id
-          label
-        }
-        publicationDate
-        publisher {
-          label
-        }
-      }
-    }
-  }
-`
-
-class EmbeddedPublicationList extends LitElement {
-
-  static get properties() {
-    return {
-      publications: { type: Array },
-      linkDecorate: { attribute: 'link-decorate', type: Boolean }
-    }
-  }
-
-  constructor() {
-    super();
-    this.publications = [];
-    this.linkDecorate = false;
-  }
-
-  connectedCallback() {
-    super.connectedCallback();
-    const data = client.query({
-      query: PUBLICATION_QUERY,
-      variables: {
-        id: this.getAttribute("person_id")
-      }
-    }).then(({data}) =>  {
-      this.publications = data.person.publications
-    });
-  }
-
-  authorTemplate(author) {
-    return html`
-      <vivo-publication-author profile-url="/entities/person/${author.id}">
-        ${author.label}
-      </vivo-publication-author>
-    `
-  }
-
-  publisherTemplate(publisher) {
-    if (publisher) {
-      return html`
-        <span slot="publisher">${publisher.label}</span>
-      `
-    }
-  }
-
-  abstractTemplate(abstract) {
-    if (abstract) {
-      return html`
-      <vivo-truncated-text slot="abstract">${abstract}</vivo-truncated-text>
-      `
-    }
-  }
-
-  publicationTemplate(p) {
-    let pubDate = new Date(p.publicationDate);
-    // TODO: would probably want to get locale from something
-    let dateFormatted = pubDate.toLocaleDateString("en-US");;
-
-    return html`
-      <vivo-publication publication-url="/entities/publication/${p.id}" 
-          link-decorate="${this.linkDecorate}"
-          published-date="${p.publicationDate}">
-        <div slot="title">${p.title}</div>
-        <vivo-publication-author-list slot="authors">
-        ${p.authors.map((a) => this.authorTemplate(a))}
-        </vivo-publication-author-list>
-        ${this.publisherTemplate(p.publisher)}
-        <span slot="date">${dateFormatted}</span>
-        ${this.abstractTemplate(p.abstractText)}
-      </vivo-publication>
-    `
-  }
-
-  render() {
-    let publicationElements = this.publications.map((p) => this.publicationTemplate(p));
-    return html`
-      <vivo-publication-list>
-        ${publicationElements}
-      </vivo-publication-list>
-    `
-  }
-
-}
-
-customElements.define('vivo-embedded-publication-list', EmbeddedPublicationList);
-*/
 import { LitElement, html, css } from 'lit-element';
 import './elements/publication';
 import './elements/publication-list';
 import './elements/publication-author-list';
 import './elements/publication-author';
+
+import './elements/sortable-list';
+import './elements/sort-option';
+
+
 import {unsafeHTML} from 'lit-html/directives/unsafe-html.js';
 
 
@@ -198,7 +73,6 @@ class EmbeddedPublicationList extends LitElement {
 
   connectedCallback() {
     super.connectedCallback();
-    console.log("hello")
     //let person_url = this.getAttribute("person-url");
     let person_id = this.getAttribute("person-id");
     //let regex = /[n]\d+/g;
@@ -259,14 +133,22 @@ class EmbeddedPublicationList extends LitElement {
     `
   }
 
-
-
-
   render() {
     let publicationElements = this.publications.map((p) => this.publicationTemplate(p));
+    // .sorts="${this.sorts}"
     return html`
-      <vivo-sortable-list item-type="publications" sortProperty="publishedDate" sortDirection="desc" .sorts="${this.sorts}">
-        ${publicationElements}
+      <vivo-sortable-list item-type="publications" sortProperty="publishedDate" sortDirection="desc">
+      <!-- allow overrides? -->
+      <vivo-sort-option field="publishedDate" direction="asc" label="Oldest First"></vivo-sort-option>
+      <vivo-sort-option field="publishedDate" direction="desc" label="Newest First"></vivo-sort-option>
+      <vivo-sort-option field="title" direction="asc" label="Publications a-z"></vivo-sort-option>
+      <vivo-sort-option field="title" direction="desc" label="Publications z-a"></vivo-sort-option>
+  
+      ${publicationElements}
+
+      <vivo-i18n-label key="showing" label="showing"></vivo-i18n-label>
+      <vivo-i18n-label key="of" label="of"></vivo-i18n-label>
+      <vivo-i18n-label key="showing_all" label="Showing all"></vivo-i18n-label>
       </vivo-sortable-list>
     `
   }
