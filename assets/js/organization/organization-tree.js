610eb4fd82de83cadaed8ea87408d2dd440d978c
Rob Nelson <robert.nelson@duke.edu>
2020-06-04 08:55:29 -0400

    #242: make organization tree also use select css

--- assets/js/organization/organization-tree.js
+++ assets/js/organization/organization-tree.js
@@ -5,8 +5,6 @@ import { until } from 'lit-html/directives/until'
 import organizationSubOrgQuery from "./organization-query"
 import OrganizationCache from "./organization-cache"
 import client from "../lib/apollo"
-import '../elements/vaadin-theme.js'
-import '@vaadin/vaadin-select'
 import _ from 'lodash'
 import qs from 'qs'
 
@@ -55,12 +53,9 @@ class OrganizationTreeListSelector extends LitElement {
   handleSelection (e) {
     let vm = this
     let selected = e.target.value
-    // console.log('selected: ' + selected)
     if (vm.treeList) {
-      // console.log('tree-list-id: ' + vm.treelistid)
       if (selected === 'list') {
         this.selected = vm.labelForSelectTypeList
-        // console.log('setting treelist attribute showas to list')
         vm.treeList.showas = 'list'
       } else {
         this.selected = vm.labelForSelectTypeTree
@@ -70,20 +65,76 @@ class OrganizationTreeListSelector extends LitElement {
       this.selected = vm.labelForSelectTypeTree
     }
   }
-  render () {
-    // console.log('treeListId: ' + this.treelistid)
-    let vm = this
-    //return html`<select @change="${this.handleSelection}"><option value="tree">Hierarchy</option><option value="list">Index</option></input>`
-    return html`
-      <vaadin-select placeholder="${vm.labelForSelectTypeTree}" value="${vm.labelForSelectTypeTree}" @value-changed="${vm.handleSelection}">
-      <template>
-        <vaadin-list-box>
-          <vaadin-item value="tree">${vm.labelForSelectTypeTree}</vaadin-item>
-          <vaadin-item value="list">${vm.labelForSelectTypeList}</vaadin-item>
-        </vaadin-list-box>
-      </template>
-      </vaadin-select>
+
+  static get styles () {
+    return css`
+    /* https://www.filamentgroup.com/lab/select-css.html */ 
+    select {
+      display: inline-block;
+      font-size: 16px;
+      font-family: sans-serif;
+      font-weight: 700;
+      color: #444;
+      line-height: 1.3;
+      padding: .6em 1.4em .5em .8em;
+      width: auto;
+      max-width: 100%;
+      box-sizing: border-box;
+      margin: 0;
+      border: 1px solid #aaa;
+      box-shadow: 0 1px 0 1px rgba(0,0,0,.04);
+      border-radius: .5em;
+      -moz-appearance: none;
+      -webkit-appearance: none;
+      appearance: none;
+      background-color: #fff;
+      background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'),
+        linear-gradient(to bottom, #ffffff 0%,#e5e5e5 100%);
+      background-repeat: no-repeat, repeat;
+      background-position: right .7em top 50%, 0 0;
+      background-size: .65em auto, 100%;
+    }
+    .select::-ms-expand {
+      display: none;
+    }
+    select:hover {
+      border-color: #888;
+    }
+    select:focus {
+      border-color: #aaa;
+      box-shadow: 0 0 1px 3px rgba(59, 153, 252, .7);
+      box-shadow: 0 0 0 3px -moz-mac-focusring;
+      color: #222;
+      outline: none;
+    }
+    select option {
+      font-weight:normal;
+    }
+    
+    /* Support for rtl text, explicit support for Arabic and Hebrew */
+    *[dir="rtl"] select, :root:lang(ar) select :root:lang(iw) select {
+      background-position: left .7em top 50%, 0 0;
+      padding: .6em .8em .5em 1.4em;
+    }
+    
+    /* Disabled styles */
+    select:disabled, select[aria-disabled=true] {
+      color: graytext;
+      background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22graytext%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'),
+        linear-gradient(to bottom, #ffffff 0%,#e5e5e5 100%);
+    }
+    select:disabled:hover, select[aria-disabled=true] {
+      border-color: #aaa;
+    }
     `
+  }
+
+  render () {
+    let vm = this
+    return html`<select @change="${this.handleSelection}">
+      <option value="tree">${vm.labelForSelectTypeTree}</option>
+      <option value="list">${vm.labelForSelectTypeList}</option>
+      </select>`
   }
 }
 customElements.define('vivo-organizations-tree-list-selector', OrganizationTreeListSelector)
