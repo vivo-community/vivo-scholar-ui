714cd621e4516dc5d96745270d7081611841351e
Rob Nelson <robert.nelson@duke.edu>
2020-06-09 15:52:11 -0400

    260: factor out proxy code into function

--- actions/app.go
+++ actions/app.go
@@ -2,7 +2,6 @@ package actions
 
 import (
 	"fmt"
-
 	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/envy"
 	forcessl "github.com/gobuffalo/mw-forcessl"
@@ -25,6 +24,27 @@ var ENV = envy.Get("GO_ENV", "development")
 var app *buffalo.App
 var T *i18n.Translator
 
+func graphqlProxy() http.Handler {
+	baseURL, err := envy.MustGet("GRAPHQL_ENDPOINT_BASE")
+	if err != nil {
+		panic(err)
+	}
+	origin, _ := url.Parse(baseURL)
+	reverseProxy := httputil.NewSingleHostReverseProxy(origin)
+	reverseProxy.ErrorHandler = func(rw http.ResponseWriter, r *http.Request, err error) {
+		fmt.Printf("error was: %+v", err)
+		rw.WriteHeader(http.StatusInternalServerError)
+		rw.Write([]byte(err.Error()))
+	}
+
+	mux := mux.NewRouter()
+	mux.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r.Host = r.URL.Host
+		reverseProxy.ServeHTTP(w, r)
+	}).Methods("GET", "POST")
+	return mux
+}
+
 // App is where all routes and middleware for buffalo
 // should be defined. This is the nerve center of your
 // application.
@@ -39,8 +59,11 @@ var T *i18n.Translator
 // placed last in the route declarations, as it will prevent routes
 // declared after it to never be called.
 func App() *buffalo.App {
-
-	baseURL, _ := envy.MustGet("GRAPHQL_ENDPOINT_BASE")
+	/*
+	baseURL, err := envy.MustGet("GRAPHQL_ENDPOINT_BASE")
+	if err != nil {
+		panic(err)
+	}
 	origin, _ := url.Parse(baseURL)
 	reverseProxy := httputil.NewSingleHostReverseProxy(origin)
 	reverseProxy.ErrorHandler = func(rw http.ResponseWriter, r *http.Request, err error) {
@@ -51,11 +74,10 @@ func App() *buffalo.App {
 
 	mux := mux.NewRouter()
 	mux.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// r.URL.Path = "/graphql"
 		r.Host = r.URL.Host
 		reverseProxy.ServeHTTP(w, r)
 	}).Methods("GET", "POST")
-
+    */
 	if app == nil {
 		app = buffalo.New(buffalo.Options{
 			Env:         ENV,
@@ -75,7 +97,8 @@ func App() *buffalo.App {
 		// Setup and use translations:
 		app.Use(translations())
 
-		app.Mount("/api/", mux)
+		//app.Mount("/api/", mux)
+		app.Mount("/api/", graphqlProxy())
 
 		app.GET("/", HomeHandler)
 		app.GET("/sitemap.xml", SiteMapHandler)
